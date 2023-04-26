ab785a6953efc97a9589a38d72e2d685553aedc6
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-04-26 12:38:29 +0300

    Added google login to the application

--- src/components/Modal/Auth/OauthButtons.tsx
+++ src/components/Modal/Auth/OauthButtons.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Button, Flex, Image } from '@chakra-ui/react';
+import { Button, Flex, Image, Text } from '@chakra-ui/react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '@/src/firebase/client.App';
+
 
 
 
 const OauthButtons:React.FC = () => {
+
+    const [
+        signInWithGoogle,
+        user,
+        loading, 
+        error,
+    ] = useSignInWithGoogle(auth)
+
+    const loginWithGoogle = async () => {
+
+        await signInWithGoogle()
+    }
     
     return(
         <Flex direction='column' width='100%' mb={4}>
-            <Button variant='oauth' mb={2}>
+            <Button variant='oauth' mb={2}  isLoading={loading} onClick={loginWithGoogle}>
                 <Image src='/images/googlelogo.png' height='20px' mr={4} alt='logo'/>
                 Continue with Google 
             </Button>
-            <Button variant='oauth' mb={2}>
+            <Button variant='oauth' mb={2} isLoading={loading}>
                 Some Other Provider
             </Button>
+
+            {error && <Text>{error.message}</Text>}
         </Flex>    )
 }
 export default OauthButtons;
--- src/firebase/client.App.ts
+++ src/firebase/client.App.ts
@@ -6,6 +6,13 @@ import {getStorage} from 'firebase/storage'
 import {getDatabase} from 'firebase/database'
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+
   apiKey: "AIzaSyDemgvj9TjTGMyas2nMrDlGO9EzTUj33KU",
   authDomain: "red-social-57bee.firebaseapp.com",
   projectId: "red-social-57bee",
